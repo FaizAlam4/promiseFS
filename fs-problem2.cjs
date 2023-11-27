eaebd4bcf52749b0e3d2cf0fb61cc90508385294
FaizAlam4 <faizalam4e@gmail.com>
2023-11-27 16:12:56 +0530

    removed unnecessary nested then()

--- fs-problem2.cjs
+++ fs-problem2.cjs
@@ -4,73 +4,47 @@ let myFunc = () => {
   let appendMyFile = (filePath, nameOfFile) => {
     fs.appendFile(filePath, nameOfFile)
       .then(() => {
-        fs.readFile("./newLipsum.txt", "utf-8")
-          .then((data) => {
-            data = data.toLowerCase();
-
-            let arr = data.split(".");
-            arr = arr.join("\n");
-            fs.writeFile("./newLipsum2.txt", arr)
-              .then(() => {
-                console.log("lipsum2");
-                fs.appendFile("./fileName.txt", "./newLipsum2.txt")
-                  .then(() => {
-                    fs.readFile("./newLipsum2.txt", "utf-8")
-                      .then((data) => {
-                        let dataArray = data.split("\n");
-                        dataArray.sort();
-                        let myData = dataArray.join("\n").trim();
-
-                        fs.writeFile("./newLipsum3.txt", myData)
-                          .then(() => {
-                            fs.appendFile("./fileName.txt", "./newLipsum3.txt")
-                              .then(() => {
-                                fs.readFile("./fileName.txt", "utf-8")
-                                  .then((data) => {
-                                    data = data.split("./");
-                                    console.log(data);
-                                    for (let myData of data) {
-                                      if (myData != "newLipsum.txt" && myData!='') {
-                                        fs.unlink(myData, (err) => {
-                                          if (err) console.log(err);
-                                          return;
-                                        });
-                                      }
-                                    }
-                                  })
-                                  .catch((err) => {
-                                    console.log("Error1:", err);
-                                  });
-                              })
-                              .catch((err) => {
-                                console.log("Error2:", err);
-                              });
-                          })
-                          .catch((err) => {
-                            console.log("Error3:", err);
-                          });
-                      })
-                      .catch((err) => {
-                        console.log("Error4:", err);
-                      });
-                  })
-                  .catch((err) => {
-                    console.log("Error5:", err);
-                  });
-              })
-              .catch((err) => {
-                console.log("Error6:", err);
-              });
-          })
-          .catch((err) => {
-            console.log("Error7:", err);
-          });
+        return fs.readFile("./newLipsum.txt", "utf-8");
+      })
+      .then((data) => {
+        data = data.toLowerCase();
+        let arr = data.split(".");
+        arr = arr.join("\n");
+        return fs.writeFile("./newLipsum2.txt", arr);
+      })
+      .then(() => {
+        return fs.appendFile("./fileName.txt", "./newLipsum2.txt");
+      })
+      .then(() => {
+        return fs.readFile("./newLipsum2.txt", "utf-8");
+      })
+      .then((data) => {
+        let dataArray = data.split("\n");
+        dataArray.sort();
+        let myData = dataArray.join("\n").trim();
+        return fs.writeFile("./newLipsum3.txt", myData);
+      })
+      .then(() => {
+        return fs.appendFile("./fileName.txt", "./newLipsum3.txt");
+      })
+      .then(() => {
+        return fs.readFile("./fileName.txt", "utf-8");
+      })
+      .then((data) => {
+        data = data.split("./");
+        console.log(data);
+        for (let myData of data) {
+          if (myData != "newLipsum.txt" && myData != "") {
+            fs.unlink(myData).catch((err) => {
+              console.log(err);
+            });
+          }
+        }
       })
       .catch((err) => {
-        console.log("Error8:", err);
+        console.log("Error:", err);
       });
   };
-
   let writeMyFile = (name, data) => {
     fs.writeFile(name, data)
       .then(() => {
@@ -93,4 +67,5 @@ let myFunc = () => {
   };
   readMyFile("./givenContent/lipsum.txt");
 };
+
 myFunc();
